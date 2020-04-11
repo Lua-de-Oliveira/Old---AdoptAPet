6d6ed7db996dae93a8725490a0b271408b92e067
Lua-de-Oliveira <luana.deoliiveiira@gmail.com>
2020-04-11 17:20:39 -0300

    Configurações iniciais

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using AdoptAPet.Models;
+
+
+namespace AdoptAPet
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IAnimalRepository, FakeAnimalRepository>();
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Animal}/{action=List}/{id?}");
+            });
+        }
+    }
+}
